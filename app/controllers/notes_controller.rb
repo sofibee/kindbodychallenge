ca448880c378f704aa5cf26a1ef02766f92d7419
Sofia Benitez <sofia.benitez39@gmail.com>
2020-06-06 21:15:40 -0400

    removed comments

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,18 +5,10 @@ class NotesController < ApplicationController
 
   end
 
-  # TO DO:below needs to be fixed
-  # def show
-    # @note = Note.find_by(patient_id: params[:patent_id], id: params[:id])
-  #   @patient = Patient.find(params[:patient_id])
-  #   @note = Note.find(params[:id])
-  # end
-
   def create
     @patient = Patient.find(params[:patient_id])
     @note = @patient.notes.create(note_params)
 
-    # TO DO: redirect to all notes for patient?
     @note.save
       redirect_to @patient
   end
