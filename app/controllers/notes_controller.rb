0b523e285205c1f1cf6b2734619ddbf5a79947ce
Sofia Benitez <sofia.benitez39@gmail.com>
2020-06-06 21:14:47 -0400

    ability to delete a note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,11 +6,11 @@ class NotesController < ApplicationController
   end
 
   # TO DO:below needs to be fixed
-  def show
+  # def show
     # @note = Note.find_by(patient_id: params[:patent_id], id: params[:id])
-    @patient = Patient.find(params[:patient_id])
-    @note = Note.find(params[:id])
-  end
+  #   @patient = Patient.find(params[:patient_id])
+  #   @note = Note.find(params[:id])
+  # end
 
   def create
     @patient = Patient.find(params[:patient_id])
@@ -27,12 +27,16 @@ class NotesController < ApplicationController
   end
 
   def update
-    # raise params.inspect
     @note = Note.find(params[:id])
     @note.update(note_params)
 
-    @patient = Patient.find(params[:patient_id])
-    @notes = @patient.notes
+    redirect_to patient_notes_url(params[:patient_id])
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+
     redirect_to patient_notes_url(params[:patient_id])
   end
 
